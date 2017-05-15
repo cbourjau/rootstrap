8d97f382eee08a4dc9b5dc26e70cb7bf9141c4a2
cbourjau <christian.bourjau@cern.ch>
2017-05-15 17:09:38 +0200

    join histograms from different folders
    Sometimes it is necessary to split large histograms up into smaller one
    to comply with ROOT's limitation about maximal object size. This commit
    adds the possibility to merge these split histograms into one np.ndarray
    for the post processing and bootstrapping

--- rootstrap/bootstrapper.py
+++ rootstrap/bootstrapper.py
@@ -3,76 +3,192 @@ import multiprocessing
 import numpy as np
 from root_numpy import hist2array
 from rootpy.io import root_open
+from rootpy.plotting import Hist
 
-import ROOT
+from ROOT import TH1, THnBase, TList
 
 
-def _extract_hist_from_path(args):
-    obj, path = args
-    if isinstance(obj, str):
-        with root_open(obj, 'READ') as f:
-            return _extract_hist_from_path([f, path])
-    head = path.split(".")[0]
-    tail = path.split(".")[1:]
-    next_obj = getattr(obj, head, None)
+def flatten_TCrap(thing):
+    """
+    Walk all paths in `thing` and yield the full path and the final histogram
+    """
+    if isinstance(thing, (TH1, THnBase, Hist)):
+        yield [thing.GetName(), hist2array(thing, return_edges=True)]
 
-    if next_obj is None:
-        next_obj = obj.FindObject(head)
-        if next_obj == None:  # TObject to Null is '==' None but not 'is' None...
-            raise ValueError("Object {} not found.".format(head))
-    if isinstance(next_obj, (ROOT.TH1, ROOT.THnBase)):
-        values, edges = hist2array(next_obj, return_edges=True)
+    for el in thing:
+        if isinstance(el, (TH1, THnBase, Hist)):
+            yield [thing.GetName() + "." + el.GetName(), hist2array(el, return_edges=True)]
+            continue
+        for k, v in flatten_TCrap(el):
+            yield el.GetName() + "." + k, v
+        # Clean up ROOT's pathetic handling of TLists...
+        if isinstance(el, TList):
+            el.Delete()
+
+
+def merge_arrays(flat_dict, hist_name, target_dim=None):
+    """
+    Due to restrictions on the grid, multiplicity is binned over
+    different histograms. This function removes the histogram from the
+    given dict!!!
+
+    Parameters
+    ----------
+    flat_dict : dict
+        Dictionary reflecting the flattened root-file folder structure
+    hist_name : str
+        Name of the histogram which will be merged over different TLists (eg. 'pairs_itsits')
+    target_dim : int
+        The dimension which represents the merged TLists (eg. -2 for mult), if None, create new dimension
+
+    Returns
+    -------
+    list :
+        List of [values, edges]
+    """
+    def compute_new_edges(_edges):
+        if target_dim is None:
+            # We have no information about the binning of that new dimension
+            new_e = np.arange(0, len(_edges) + 1)
+        else:
+            # The information for the new bins is already saved in one of the existing
+            # dimensions of the original histogram
+            new_e = None
+            for e in _edges:
+                if new_e is None:
+                    new_e = list(e)
+                else:
+                    new_e.append(e[1])
+            new_e = np.array(new_e)
+        all_edges = _edges[0]
+        if target_dim:
+            all_edges[target_dim] = new_e
+            return all_edges
+        else:
+            return [new_e, all_edges]
+
+    sorted_keys = sorted([k for k in flat_dict.keys() if hist_name in k])
+    _vals, _edges = zip(*[flat_dict[k] for k in sorted_keys])
+    if target_dim:
+        _vals = np.concatenate(_vals, target_dim)
     else:
-        values, edges = _extract_hist_from_path([next_obj, ".".join(tail)])
-    # delete list before closing file because... root...
-    if isinstance(next_obj, ROOT.TList):
-        next_obj.Delete()
-    return values, edges
+        _vals = np.stack(_vals, axis=0)
+    return _vals, compute_new_edges(_edges)
+
+
+# def _extract_hist_from_path(args):
+#     obj, path = args
+#     if isinstance(obj, str):
+#         with root_open(obj, 'READ') as f:
+#             return _extract_hist_from_path([f, path])
+#     head = path.split(".")[0]
+#     tail = path.split(".")[1:]
+#     next_obj = getattr(obj, head, None)
+
+#     if next_obj is None:
+#         next_obj = obj.FindObject(head)
+#         if next_obj == None:  # TObject to Null is '==' None but not 'is' None...
+#             raise ValueError("Object {} not found in {}.".format(head, obj))
+#     if isinstance(next_obj, (ROOT.TH1, ROOT.THnBase)):
+#         values, edges = hist2array(next_obj, return_edges=True)
+#     else:
+#         values, edges = _extract_hist_from_path([next_obj, ".".join(tail)])
+#     # delete list before closing file because... root...
+#     if isinstance(next_obj, ROOT.TList):
+#         next_obj.Delete()
+#     return values, edges
 
 
 class Sample_set():
-    def __init__(self, path, bootstrapper):
-        self.path = path
-        self.bootstrapper = bootstrapper
+    def __init__(self, bootstrapper, edges, first_value):
+        self.edges = edges
+        self._bs = bootstrapper
+        # number of value_sets appended to this sample set
+        self._filled_sets = 0
+        self.values_set = np.zeros(shape=(first_value.shape + (len(self._bs.files), )),
+                                   dtype=first_value.dtype)
+        self.values_set[..., self._filled_sets] = first_value
+
+    def append(self, values):
+        """
+        Append a set of values (e.g. the results of a run) to this sample set
+        """
+        self._filled_sets += 1
+        self.values_set[..., self._filled_sets] = values
 
     def values(self):
         try:
-            return np.sum(self.bootstrapper.current_weights * self.values_set, axis=-1, dtype=np.float64)
+            return np.sum(self._bs.current_weights * self.values_set.astype(np.float64),
+                          axis=-1)
         except AttributeError:
             raise RuntimeError("Call Bootstrapper.draw first")
 
-    def read_files(self, files):
-        with root_open(files[0], 'READ') as f:
-            values, self.edges = _extract_hist_from_path([f, self.path])
-        self.values_set = np.zeros(shape=(values.shape + (len(files), )), dtype=np.float64)
-        nworkers = 10
-        pool = multiprocessing.Pool(nworkers)
-        values_edges = pool.map(_extract_hist_from_path, zip(files, [self.path] * len(files)))
-        for iset, (values, _) in enumerate(values_edges):
-            self.values_set[..., iset] = values
-        # for iset, fname in enumerate(files):
-        #     def _worker():
-        #         with root_open(fname, 'READ') as f:
-        #             self.values_set[..., iset], _ = extract_hist_from_path(f, self.path)
-        #     t = threading.Thread(target=_worker)
-        #     t.start()
-        # for t in threads:
-        #     t.join()
-        pool.close()
+    # def read_files(self, files):
+    #     with root_open(files[0], 'READ') as f:
+    #         values, self.edges = _extract_hist_from_path([f, self.path])
+    #     self.values_set = np.zeros(shape=(values.shape + (len(files), )), dtype=values.dtype)
+    #     nworkers = 9
+    #     pool = multiprocessing.Pool(nworkers)
+    #     values_edges = pool.map(_extract_hist_from_path, zip(files, [self.path] * len(files)))
+    #     for iset, (values, _) in enumerate(values_edges):
+    #         self.values_set[..., iset] = values
+    #     pool.close()
+
+
+def _fixed_arrays_from_file(args):
+    (fname, root_dir, dim_from_dirnames) = args
+    with root_open(fname, 'READ') as f:
+        # This probably breaks if there is more then one level of depth!
+        if root_dir == ".":
+            thing = f
+        else:
+            thing = f.__getattr__(root_dir)
+        # get a flat dictionary of all the histograms below the root directory
+        d = {k: v for k, v in flatten_TCrap(thing)}
+    # find all the histogram names which we want to register / keep
+    hnames = list(set([k.split('.')[-1] for k in d.keys()]))
+    # FIXME: merge the multiplicity dimension, which is currently in the folder hirachy,
+    # into the ndarray. Clearly, this shouldn't depend on my analysis!
+    merged_d = {hname: merge_arrays(d, hname, dim_from_dirnames) for hname in hnames}
+    return merged_d
 
 
 class Bootstrapper():
-    def __init__(self):
+    """
+    This class ties together logically dependent `Sample_set`s. I.e.,
+    we might have a various histograms which were produced in parallel
+    like a "eventCounter" and a "particle pair counter". Individual
+    sample sets are registered with the `register` function. The IO is
+    derefered until the `read_files` function is called.
+    """
+    def __init__(self, files, root_dir, dim_from_dir_names=None):
+        self.files = files
+        self.root_dir = root_dir
         self.sample_sets = []
+        self._dim_from_dir_names = dim_from_dir_names
 
-    def register(self, name, path):
-        setattr(self, name, Sample_set(path, self))
-        self.sample_sets.append(getattr(self, name))
+    # def register(self, name, path):
+    #     setattr(self, name, Sample_set(path, self))
+    #     self.sample_sets.append(getattr(self, name))
 
-    def read_files(self, files):
-        self.nsamples = len(files)
-        for s in self.sample_sets:
-            s.read_files(files)
+    def read_files(self):
+        self.nsamples = len(self.files)
+        nworkers = 9
+        pool = multiprocessing.Pool(nworkers)
+        # create a list of dicts containing the "fixed" histograms
+        dict_merged_hists = pool.map(_fixed_arrays_from_file,
+                                     zip(self.files,
+                                         [self.root_dir] * len(self.files),
+                                         [self._dim_from_dir_names] * len(self.files)))
+        pool.close()
+        while dict_merged_hists:
+            merged_d = dict_merged_hists.pop()
+            while merged_d:
+                hname, (values, edges) = merged_d.popitem()
+                try:
+                    getattr(self, hname).append(values)
+                except AttributeError:
+                    setattr(self, hname, Sample_set(self, edges=edges, first_value=values))
 
     def draw(self):
         indices, counts = np.unique(np.random.choice(self.nsamples, size=self.nsamples),
--- rootstrap/tests.py
+++ rootstrap/tests.py
@@ -5,37 +5,40 @@ import numpy as np
 
 from rootstrap import Bootstrapper, Collector
 from rootpy.io import root_open
-from rootpy.plotting import Hist1D
-from ROOT import TList, TObject
+from rootpy.plotting import Hist1D, Hist2D
+
+NBINS_FROM_FOLDER = 3
 
 
 class Test_end2end(TestCase):
     def setUp(self):
-        for i in range(5):
-            tl = TList()
-            with root_open("AnalysisResults{}.root".format(i), 'RECREATE') as f:
-                f.mkdir("dir")
-                f.cd("dir")
-                h_proton = Hist1D(3, 0, 3, name="proton_dist")
-                h_pion = Hist1D(3, 0, 3, name="pion_dist")
+        for ifile in range(5):
+            with root_open("AnalysisResults{}.root".format(ifile), 'RECREATE') as f:
+                for i in range(NBINS_FROM_FOLDER):
+                    f.mkdir("dir{}".format(i))
+                    f.cd("dir{}".format(i))
+                    h_proton = Hist1D(3, 0, 3, name="proton_dist")
+                    h_pion = Hist1D(3, 0, 3, name="pion_dist")
 
-                # + 1 so that we don't divide by 0 later on
-                [h_proton.Fill(x, i + 1) for x in range(h_proton.GetNbinsX())]
-                [h_pion.Fill(x, i + 1) for x in range(h_proton.GetNbinsX())]
-                tl.Add(h_proton)
-                tl.Add(h_pion)
-                tl.Write("list")
+                    # + 1 so that we don't divide by 0 later on
+                    [h_proton.Fill(x, ifile + 1) for x in range(h_proton.GetNbinsX())]
+                    [h_pion.Fill(x, ifile + 1) for x in range(h_proton.GetNbinsX())]
+                    h_proton.Write()
+                    h_pion.Write()
 
     def test_end2end(self):
         files = glob("./AnalysisResults*.root")
 
-        bs = Bootstrapper()  # set other options here as well
-        bs.register("proton_dist", path="dir.list.proton_dist")
-        bs.register("pion_dist", path="dir.list.pion_dist")
-        bs.read_files(files)
+        bs = Bootstrapper(files, root_dir=".", dim_from_dir_names=None)
+        bs.read_files()
 
         proton_collector = Collector()
         ratio_collector = Collector()
+
+        bs.draw()  # set up a new bootstrapped sample
+        # Check resulting shape; compare with setUp()
+        self.assertEqual(bs.proton_dist.values().shape, (NBINS_FROM_FOLDER, 3))
+        # print bs.pion_dist.values_set
         for i in range(10000):
             bs.draw()  # set up a new bootstrapped sample
             proton_collector.add(bs.proton_dist.values())
@@ -45,15 +48,63 @@ class Test_end2end(TestCase):
         mean, sigma = proton_collector.mean(), proton_collector.sigma()
         self.assertTrue(np.all(mean - sigma < np.array([15, 15, 15])))
         self.assertTrue(np.all(mean + sigma > np.array([15, 15, 15])))
-        np.testing.assert_array_equal(ratio_collector.mean(), np.array([1., 1., 1.]))
+        np.testing.assert_array_equal(ratio_collector.mean(),
+                                      np.array([[1., 1., 1.]] * NBINS_FROM_FOLDER))
+
+
+class Test_end2end_dim_exists_in_original_hists(TestCase):
+    def setUp(self):
+        for ifile in range(5):
+            with root_open("AnalysisResults{}.root".format(ifile), 'RECREATE') as f:
+                for i in range(NBINS_FROM_FOLDER):
+                    f.mkdir("dir{}".format(i))
+                    f.cd("dir{}".format(i))
+                    h_proton = Hist2D(3, 0, 3, 1, i * 10, (i + 1) * 10, name="proton_dist")
+                    h_pion = Hist2D(3, 0, 3, 1, i * 10, (i + 1) * 10, name="pion_dist")
+
+                    # + 1 so that we don't divide by 0 later on
+                    [h_proton.Fill(x, i * 10, ifile + 1) for x in range(h_proton.GetNbinsX())]
+                    [h_pion.Fill(x, i * 10, ifile + 1) for x in range(h_proton.GetNbinsX())]
+                    h_proton.Write()
+                    h_pion.Write()
+
+    def test_end2end(self):
+        files = glob("./AnalysisResults*.root")
+
+        bs = Bootstrapper(files, root_dir=".", dim_from_dir_names=1)
+        bs.read_files()
+
+        proton_collector = Collector()
+        ratio_collector = Collector()
+
+        bs.draw()  # set up a new bootstrapped sample
+        # Check resulting shape; compare with setUp()
+        self.assertEqual(bs.proton_dist.values().shape, (3, NBINS_FROM_FOLDER))
+        # print bs.pion_dist.values_set
+        for i in range(10000):
+            bs.draw()  # set up a new bootstrapped sample
+            proton_collector.add(bs.proton_dist.values())
+            ratio_collector.add(bs.proton_dist.values() / bs.pion_dist.values())
+
+        # 5 samples; each bin is sum(range(5) + 1) == 15
+        mean, sigma = proton_collector.mean(), proton_collector.sigma()
+        self.assertTrue(np.all(mean - sigma < np.array([15, 15, 15])))
+        self.assertTrue(np.all(mean + sigma > np.array([15, 15, 15])))
+        np.testing.assert_array_equal(ratio_collector.mean(),
+                                      np.array([[1., 1., 1.]] * NBINS_FROM_FOLDER))
 
 
 class Test_collector(TestCase):
     def test_mean(self):
         points = np.linspace(0, 10, num=50)
-        print points
         c = Collector()
         [c.add([p]) for p in points]
         self.assertEqual(np.mean(points), c.mean()[0])
         self.assertLessEqual(np.std(points) * 0.98, c.sigma()[0])
         self.assertLessEqual(c.sigma()[0], np.std(points) * 1.02)
+
+    def test_as_hist(self):
+        points = np.linspace(0, 10, num=1000).reshape((10, 10, 10))
+        c = Collector(edges=[range(10 + 1), range(10 + 1)])
+        [c.add(p) for p in points]
+        c.as_root_hist()
