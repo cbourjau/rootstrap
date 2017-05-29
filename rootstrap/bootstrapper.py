02a5a71ef47f438bfe897c093573138d228e9989
cbourjau <christian.bourjau@cern.ch>
2017-05-29 18:44:00 +0200

    Start to move logic of Observables into rootstrap
    The hope is to make the post analysis more linear:
    
    grid -> bootstrap -> dict of Observables (`Collectors`)
    
    Currently, there is one more indirection in terms of the undefiable
    `Analyzer` thing...
    
    Also, this might make it possible to hide more of the inner workings of
    the bootstrapping process to the user

--- rootstrap/bootstrapper.py
+++ rootstrap/bootstrapper.py
@@ -50,31 +50,118 @@ class Sample_set():
         values_edges = pool.map(_extract_hist_from_path, zip(files, [self.path] * len(files)))
         for iset, (values, _) in enumerate(values_edges):
             self.values_set[..., iset] = values
-        # for iset, fname in enumerate(files):
-        #     def _worker():
-        #         with root_open(fname, 'READ') as f:
-        #             self.values_set[..., iset], _ = extract_hist_from_path(f, self.path)
-        #     t = threading.Thread(target=_worker)
-        #     t.start()
-        # for t in threads:
-        #     t.join()
         pool.close()
 
 
 class Bootstrapper():
     def __init__(self):
         self.sample_sets = []
+        # list of all registered sources as `Sample_set`
+        self.sources = []
+        # list of all static sources and their set-up callbacks (name, callback)
+        self.static_sources = []
+        # list of all registered observable callbacks as tuples: (name, callback)
+        # This has to be a list since the order matters
+        self._obs_callback = []
+        # Dictionary of all the observables `Collector`s
+        self._obs_collectors = {}
+        # Function called to compute sample weights
+        self.sample_weight = lambda cls: 1
 
-    def register(self, name, path):
+    def register_histogram_source(self, name, path):
+        """
+        Register a histogram which is present in all the given files and consitutes a sums
+
+        Parameters
+        ----------
+        name : str
+            The name under which the given source will be accessable
+        path : Path to the histogram within the root file
+        """
         setattr(self, name, Sample_set(path, self))
         self.sample_sets.append(getattr(self, name))
+
+    def register_observable(self, name, edges, callback):
+        """
+        Register an observable which will be calculated in each iteration
+
+        Parameters
+        ----------
+        name : str
+            Name of this observable; must be unique. If other
+            calculations depend on this value, it can be accessed
+            throught this name
+        edges : str, list, function
+            Edges of this observable. If `str`, import edges from a
+            source histogram of this name. If `list` of nd.array, use
+            these values as bin edges. If `function` (this)->(list), use this
+            function to calculate bin edges. Called once.
+        callback : fn (this) -> ndarray
+            Called once per iteration. Must return an nd.array of
+            dimensionality fitting to the observables bin edges.
+        """
+        pass
+
+    def register_static_source(self, name, callback):
+        """
+        Register a souce which is static over all iterations but is
+        needed for the calculation of an observable. I.e., dead
+        regions of a detector or efficiencies.
+
+        Parameters
+        ----------
+        name : str
+            The name under which the given source will be accessable
+        callback : function
+            Called once with access to all the histograms. Must return `[values, edges]`
+        """
+        self.static_sources.append((name, callback))
+
+    def register_sample_weight(self, callback):
+        """
+        Register the logic for calculating a given iteration's sample
+        weight. If this function is not called, all samples are
+        weighted equally. Usually one would like to weight a sample by
+        e.g. number of events
+
+        Parameters
+        ----------
+        callback : function
+            Function called once per iteration. Must return a scalar value
+        """
+        self.sample_weight = callback
+
+    def bootstrap(self, ntimes):
+        """
+        Bootstrap this configuration `ntimes`.
+
+        Parameters
+        ----------
+        ntimes : int
+            Number of iterations
+
+        Returns
+        -------
+        dict : Dictionary of all the registered observables
+        """
+        for n in range(ntimes):
+            self._draw()
+            # dictionary with values of observables from the current iteration
+            self.current_observables = {}
+            for name, cback in self._obs_callbacks:
+                self.current_observables[name] = cback(self)
+
+        result = {}
+        for name, col in self._obs_collectors:
+            result[name] = col
+        return result
 
     def read_files(self, files):
         self.nsamples = len(files)
         for s in self.sample_sets:
             s.read_files(files)
 
-    def draw(self):
+    def _draw(self):
         indices, counts = np.unique(np.random.choice(self.nsamples, size=self.nsamples),
                                     return_counts=True)
         # create an array with weights for each sub-sample item
